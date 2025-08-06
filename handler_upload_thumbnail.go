84c368805edd61840c85390a2bad6fd117ab2b84
Mudassir <mughalmudassir966@gmail.com>
2025-08-06 20:23:28 +0500

    feat: added the ability to store thumbnails

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/base64"
 	"fmt"
 	"io"
 	"net/http"
@@ -60,17 +61,13 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		return
 	}
 
-	videoThumbnails[videoID] = thumbnail{
-		data:      data,
-		mediaType: mediaType,
-	}
+	imageDataInString := base64.StdEncoding.EncodeToString(data)
+	dataUrl := fmt.Sprintf("data:%v;base64,%v", mediaType, imageDataInString)
 
-	url := fmt.Sprintf("http://localhost:%s/api/thumbnails/%s", cfg.port, videoID)
-	video.ThumbnailURL = &url
+	video.ThumbnailURL = &dataUrl
 
 	err = cfg.db.UpdateVideo(video)
 	if err != nil {
-		delete(videoThumbnails, videoID)
 		respondWithError(w, http.StatusInternalServerError, "failed to updated the video", err)
 		return
 	}
